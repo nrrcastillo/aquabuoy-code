0461d94cc21685f5c4a1eb5070f3ca20e7586c58
nrrcastillo <126449464+nrrcastillo@users.noreply.github.com>
2023-08-11 16:26:27 -0700

    Delete EF.m

--- EF.m
+++
@@ -1,6 +0,0 @@
-% File EF.m containing returns the exciting force Fw, for a given: t,A,T,F
-function Fw = EF(t,A,T,F)
-
-omega = 2*pi/T;              % For wave period, corresponding wave frequency
-gamma = 0;                   % Arbitrary phase, choose 0
-Fw = A*F*cos(omega*t+gamma); % exciting force function
